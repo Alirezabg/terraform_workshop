dd6716703821969881877c792f1708cd5bca8b61
Alireza Bagheri <alirezabg@gmail.com>
2022-11-09 22:00:29 +0000

    add feature to terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,84 @@
 ##################
 # CONFIGURATION
 ##################
+variable "project_id" {
+    type = string
+}
+
+locals {
+  project_id = var.project_id
+}
+provider "google" {
+  version = "3.5.0"
+  project     = local.project_id
+  region      = "us-central1"
+  zone = "us-central1-c"
+}
+resource "google_project_service" "compute_service" {
+  project = local.project_id
+  service = "compute.googleapis.com"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network-cyf"
+  auto_create_subnetworks = false
+  delete_default_routes_on_create = true
+    depends_on = [
+    google_project_service.compute_service
+  ]
+}
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
+}
+resource "google_compute_instance" "vm_instance" {
+name = "api-1"
+machine_type = "f1-micro"
+zone = "us-central1-c"
+tags         = ["ssh"]
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-11"
+}
+}
+resource "google_compute_instance" "vm_instance" {
+name = "tapi-2"
+machine_type = "f1-micro"
+zone = "us-central1-c"
+  tags         = ["ssh"]
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-11"
+}
+}
+resource "google_compute_instance" "vm_instance" {
+name = "flask-app"
+machine_type = "f1-micro"
+zone = "us-central1-c"
+tags = ["ssh"]
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-11"
+}
+}
+metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+network_interface {
+network = google_compute_network.vpc_network.name
+access_config {
+}
+}
+}
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {
+  description = "The ID to give the project. If not provided, the `name` will be used."
+  type        = string
+  default     = "titanium-acumen-367712"
+}
